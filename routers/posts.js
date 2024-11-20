d3c0aaa9644ec68d013e84e0a1b3efd730324d6b
emazupp <zuppardo.emanuele2001@gmail.com>
2024-11-20 17:11:02 +0100

    bonus

--- routers/posts.js
+++ routers/posts.js
@@ -7,23 +7,41 @@ const posts = require("../db/posts");
 
 // * INDEX
 router.get("/", (req, res) => {
-  const text = "ciao";
-  res.json(text);
+  res.json(posts);
 });
 
 // * SHOW
-router.get("/:id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const filteredPost = posts.find((post) => post.id === id);
+  res.json(filteredPost);
+});
 
 // * STORE
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  const text = "Creazione nuovo post";
+  res.json(text);
+});
 
 // * UPDATE
-router.put("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const text = `Modifica totale del post con id: ${id}`;
+  res.json(text);
+});
 
 // * MODIFY
-router.patch("/:id", (req, res) => {});
+router.patch("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const text = `Modifica parziale del post con id: ${id}`;
+  res.json(text);
+});
 
 // * DESTROY
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const text = `Cancellazione del post con id: ${id}`;
+  res.json(text);
+});
 
 module.exports = router;
